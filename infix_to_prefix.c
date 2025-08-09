94361ab97baab06a66e8fec1166ffdedddef3169
Kevin Jose <mjkevin2006@gmail.com>
2025-08-09 09:06:18 +0530

    Question No.4

--- infix_to_prefix.c
+++ infix_to_prefix.c
@@ -1,5 +1,3 @@
-/*Write a program to convert an infix expression to a prefix expression using stack.*/
-
 #include <stdio.h>
 #include <math.h>
 #include <ctype.h>
@@ -47,22 +45,22 @@ int isp(char op) {
         return 4;
     else if (op == '+' || op == '-')
         return 2;
-    else if (op == '(')
+    else if (op == ')') // when scanning right-to-left, ) acts like (
         return 0;
     else
         return -1;
 }
 
 int icp(char op) {
-    if (op == '(')
-        return 7;
+    if (op == ')')
+        return 7;  // high precedence to push onto stack
     else if (op == '^')
         return 6;
     else if (op == '*' || op == '/')
         return 3;
     else if (op == '+' || op == '-')
         return 1;
-    else if (op == ')')
+    else if (op == '(') // when scanning right-to-left, ( acts like )
         return 0;
     else
         return -1;
@@ -85,72 +83,57 @@ int result(int v1, int v2, char op) {
 
 int main() {
     char str[MAX], el, x, prefix[MAX];
-    int l, i = 0, val1, val2, p = 0, r, temp;
+    int l, i, val1, val2, p = 0, r;
 
     printf("Enter your expression: ");
     scanf("%s", str);
 
     l = strlen(str);
 
-    // Reverse the infix expression
-    for (i = 0; i < l / 2; i++) {
-        temp = str[i];
-        str[i] = str[l - i - 1];
-        str[l - i - 1] = temp;
-    }
+    // Start with ')' on stack for right-to-left scan
+    push(')');
 
-    // Swap parenthesis
-    for (i = 0; i < l; i++) {
-        if (str[i] == '(')
-            str[i] = ')';
-        else if (str[i] == ')')
-            str[i] = '(';
-    }
-
-    // Add a ')' at the end
-    str[l] = ')';
-    str[l + 1] = '\0';
-
-    i = 0;
-    p = 0;
-    push('('); // Initial push to start conversion
-
+    // Scan from right to left
+    i = l - 1;
     while (top > -1) {
-        el = str[i++];
+        el = (i >= 0) ? str[i--] : '\0';
         x = pop();
 
-        if (isalnum(el)) {
+        if (isalnum(el)) { // operand
             prefix[p++] = el;
             push(x);
-        } else if (el == ')') {
-            while (x != '(') {
+        }
+        else if (el == '(') { // acts like ')' in normal L->R
+            while (x != ')') {
                 prefix[p++] = x;
                 x = pop();
             }
-        } else if (isp(x) >= icp(el)) {
-            while (isp(x) >= icp(el)) {
+        }
+        else if (isp(x) > icp(el)) {
+            while (isp(x) > icp(el)) {
                 prefix[p++] = x;
                 x = pop();
             }
-            push(x);
-            push(el);
-        } else {
             push(x);
             push(el);
         }
+        else {
+            push(x);
+            if (el != '\0') push(el);
+        }
     }
 
-    // Reverse the result to get final prefix
+    // Reverse prefix array to get correct order
     for (i = 0; i < p / 2; i++) {
-        temp = prefix[i];
+        char temp = prefix[i];
         prefix[i] = prefix[p - i - 1];
         prefix[p - i - 1] = temp;
     }
-
     prefix[p] = '\0';
+
     printf("Prefix expression: %s\n", prefix);
 
-    // Evaluate the prefix expression
+    // Evaluate prefix expression
     for (i = p - 1; i >= 0; i--) {
         if (isdigit(prefix[i])) {
             pushval(prefix[i] - '0');
