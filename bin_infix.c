740b46491ae6dd8873a0ab27111bbfdb816329a7
Kevin Jose <mjkevin2006@gmail.com>
2025-09-24 05:48:18 +0530

    Question No.11

--- bin_infix.c
+++ bin_infix.c
@@ -5,64 +5,64 @@
 
 struct node
 {
-char data;
-struct node* lchild;
-struct node* rchild;
+        char data;
+        struct node* lchild;
+        struct node* rchild;
 };
 
 void postfix(struct node* r)
 {
-if(r != NULL)
-{
-postfix(r->lchild);
-        postfix(r->rchild);
-        printf("%c",r->data);
-}
+        if(r != NULL)
+        {
+                postfix(r->lchild);
+                postfix(r->rchild);
+                printf("%c",r->data);
+        }
 }
 
 void prefix(struct node* r)
 {
-if(r != NULL)
+        if(r != NULL)
         {
                 printf("%c",r->data);
-       prefix(r->lchild);
-        prefix(r->rchild);
+                prefix(r->lchild);
+                prefix(r->rchild);
 
         }
 }
 
 void tree(struct node **r)
 {
-char val;
+        char val;
 
-printf("Enter the value(Enter $ for NULL) : ");
-scanf("%c",&val);
-getchar();
-if(val =='$')
-{
-r == NULL;
-return;
-}
+        printf("Enter the value(Enter $ for NULL) : ");
+        scanf("%c",&val);
+        getchar();
+        if(val =='$')
+        {
+                r == NULL;
+                return;
+        }
 
-struct node* n = (struct node*)malloc(sizeof(struct node));
-n->data = val;
-n->lchild = NULL;
-n->rchild = NULL;
-*r = n;
-printf("Enter the value of left child of %c: ",val);
-tree(&((*r)->lchild));
-printf("\n");
-printf("Enter the value of right child of %c: ",val);
-tree(&((*r)->rchild));
+        struct node* n = (struct node*)malloc(sizeof(struct node));
+        n->data = val;
+        n->lchild = NULL;
+        n->rchild = NULL;
+        *r = n;
+        printf("Enter the value of left child of %c: ",val);
+        tree(&((*r)->lchild));
+        printf("\n");
+        printf("Enter the value of right child of %c: ",val);
+        tree(&((*r)->rchild));
 }
 
 int main()
 {
-struct node  *root = NULL;
+        struct node  *root = NULL;
 
-tree(&root);
-postfix(root);
-printf("\n");
-prefix(root);
-return 0;
+        tree(&root);
+        postfix(root);
+        printf("\n");
+        prefix(root);
+        return 0;
 }
